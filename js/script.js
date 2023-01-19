bd5f298c71248e9da3eaad0b29215bfeb13edc6e
Yan <66394095+Yan1993-Dev@users.noreply.github.com>
2023-01-19 20:35:31 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const popup = document.querySelector(".popup");
+const openBtn = document.querySelector(".banner__btn-js");
+const closeBtn = popup.querySelector(".close");
+const firstInput = popup.querySelector(".form__input");
+
+openBtn.addEventListener('click', function() {
+    popup.classList.add('open');
+    firstInput.focus();
+});
+
+closeBtn.addEventListener('click', function() {
+    popup.classList.remove('open');
+});
+
+window.addEventListener("keydown", function(e) {
+    if(e.code === "Escape" && popup.classList.contains('open')) {
+        popup.classList.remove('open');
+    };
+});
